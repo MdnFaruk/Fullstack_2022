c1c4003d6f262f9e19db021d9d8b4ddda2b22193
Md Nuruzzaman Faruk <mdnfaruk.fin@gmail.com>
2022-03-09 13:36:07 +0200

    2.1-2.5 done

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,24 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+
+const Content = ({ parts }) => 
+  <>
+    {parts.map((value,index) => 
+        <Part part={value} key={index}/>
+      )}
+  </>
+
+const Course = ({ course }) => {
+    return (
+        <>
+        <Header course={course.name}/>
+        <Content parts={course.parts} />
+        <Total sum={course.parts.reduce((total,value) => (total + value.exercises) , 0)} />
+        </>
+    )
+}  
+
+export default Course  
